9e76d160829aae72e1f5f4c6319c8f45bbb1b8e5
sekishin <s14t242@stmail.eng.kagawa-u.ac.jp>
2016-12-18 11:45:33 +0900

    リファクタリング

--- src/figureTrace/RunThread.java
+++ src/figureTrace/RunThread.java
@@ -7,22 +7,41 @@ public class RunThread implements Runnable{
 
 	private static RegulatedMotor leftMotor  = Motor.C;
 	private static RegulatedMotor rightMotor  = Motor.B;
+	private static final int SPEED_FOR_STRAIGHT = 600;
+	private static final int SPEED_FOR_TURN = 300;
+	private static final int RIGHT_SPEED_FOR_BIG_CIRCLE = 700;
+	private static final int LEFT_SPEED_FOR_BIG_CIRCLE = 520;
+	private static final int RIGHT_SPEED_FOR_SMALL_CIRCLE = 700;
+	private static final int LEFT_SPEED_FOR_SMALL_CIRCLE = 480;
+	private static final int L1_FINISH_TIME = 18;
+	private static final int V1_FINISH_TIME = 23;
+	private static final int L2_FINISH_TIME = 53;
+	private static final int V2_FINISH_TIME = 55;
+	private static final int C1_FINISH_TIME = 95;
+	private static final int V3_FINISH_TIME = 106;
+	private static final int C2_FINISH_TIME = 134;
 
 	@Override
 	public void run() {
-		goStraight(18);
-		turn(23);
-		goStraight(53);
-		turn(55);
-		bigCircle(700, 520, 95);
-		smallCircle(700, 480, 134);
-		leftMotor.stop();
-		rightMotor.stop();
+		goStraightUnit();
+		goCircleUnit();
+	}
+
+	public void goStraightUnit() {
+		goStraight(L1_FINISH_TIME);
+		turn(V1_FINISH_TIME);
+		goStraight(L2_FINISH_TIME);
+		turn(V2_FINISH_TIME);
+	}
+
+	public void goCircleUnit() {
+		goCircle(RIGHT_SPEED_FOR_BIG_CIRCLE, LEFT_SPEED_FOR_BIG_CIRCLE, C1_FINISH_TIME);
+		turn(V3_FINISH_TIME);
+		goCircle(RIGHT_SPEED_FOR_SMALL_CIRCLE, LEFT_SPEED_FOR_SMALL_CIRCLE, C2_FINISH_TIME);
 	}
 
 	public void goStraight(int time) {
-		leftMotor.setSpeed(600);
-		rightMotor.setSpeed(600);
+		setMoterSpeed(SPEED_FOR_STRAIGHT, SPEED_FOR_STRAIGHT);
 		while (TimeThread.getTime() <= time) {
 			leftMotor.forward();
 			rightMotor.forward();
@@ -30,38 +49,29 @@ public class RunThread implements Runnable{
 	}
 
 	public void turn(int time) {
-		leftMotor.setSpeed(300);
-		rightMotor.setSpeed(300);
+		setMoterSpeed(SPEED_FOR_TURN, SPEED_FOR_TURN);
 		while (TimeThread.getTime() <= time) {
 			leftMotor.backward();
 			rightMotor.forward();
 		}
+		stopMoter();
 	}
 
-	public void  bigCircle(int rSpeed, int lSpeed, int time) {
-		leftMotor.setSpeed(lSpeed);
-		rightMotor.setSpeed(rSpeed);
+	public void  goCircle(int rSpeed, int lSpeed, int time) {
+		setMoterSpeed(rSpeed, lSpeed);
 		while (TimeThread.getTime() <= time) {
 			rightMotor.forward();
 			leftMotor.forward();
 		}
-		leftMotor.setSpeed(300);
-		rightMotor.setSpeed(300);
-		while (TimeThread.getTime() <= time+11) {
-			rightMotor.backward();
-			leftMotor.forward();
-		}
-		rightMotor.stop(true);
-		leftMotor.stop();
+		stopMoter();
 	}
 
-	public void smallCircle(int rSpeed, int lSpeed, int time) {
+	public void setMoterSpeed(int rSpeed, int lSpeed) {
 		leftMotor.setSpeed(lSpeed);
 		rightMotor.setSpeed(rSpeed);
-		while (TimeThread.getTime() <= time) {
-			rightMotor.forward();
-			leftMotor.forward();
-		}
+	}
+
+	public void stopMoter() {
 		rightMotor.stop(true);
 		leftMotor.stop();
 	}
